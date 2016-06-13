00dfc5aa0382b2943ff04acf91bb50c3349f1886
atreb <bhupendra.atre@dowjones.com>
2016-06-13 10:39:56 -0400

    Added slow endpoint

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.get('/_env', function (req, res) {
   res.json(process.env);
 });
 
+app.get('/slowendpoint', function (req, res) {
+  console.log('Got request for slow endpoint');
+  //wait for 12 sec for nginx to timeout at 10 sec
+  setTimeout(function () {
+    console.log('Sending response for slow endpoint');
+    res.json({status: '12 sec delayed response'});
+  }, 12000);
+})
+
 app.listen(PORT, function () {
   console.log('Application started on port: ' + PORT);
 });
