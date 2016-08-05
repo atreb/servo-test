239d5f22474885064c0910e5a1856dd91bb45ff9
atreb <bhupendra.atre@dowjones.com>
2016-08-05 11:22:39 -0400

    added file upload support

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require('express'),
-    app = express(),
-    PORT = process.env.PORT || 4001,
-    bodyParser = require('body-parser');
-
+var fs = require('fs'),
+  express = require('express'),
+  app = express(),
+  PORT = process.env.PORT || 4001,
+  multiParty = require('multiparty'),
+  bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
@@ -15,18 +16,34 @@ app.get('/_env', function (req, res) {
 });
 
 app.get('/slowendpoint', function (req, res) {
-  console.log('Got request for slow endpoint');
   //wait for 12 sec for nginx to timeout at 10 sec
   setTimeout(function () {
-    console.log('Sending response for slow endpoint');
     res.json({status: '12 sec delayed response'});
   }, 12000);
 });
 
 app.post('/postendpoint', function (req, res) {
-  console.log('inside post endpoint');
   res.json(req.body);
-})
+});
+
+app.post('/uploadendpoint', function (req, res) {
+  var form = new multiParty.Form();
+  form.parse(req, function (err, fields, files) {
+    if (err) return res.json({error: err.stack});
+    /* This saves the file to downloads directory in current directory
+    fs.rename(files.file[0].path, './downloads/' + files.file[0].originalFilename, function (err) {
+      if (err) return res.json({error: err.stack});
+      res.json({fields: fields, files: files});
+    });
+    */
+    fs.unlink(files.file[0].path, function (err) {
+      if (err) return res.json({error: err.stack});
+      res.json({msg: 'File was successfully uploaded to server and then deleted'});
+    });
+  });
+});
+
+app.use(express.static('static'));
 
 app.listen(PORT, function () {
   console.log('Application started on port: ' + PORT);
