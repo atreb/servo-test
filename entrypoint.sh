b1591f21efa28971057e4bb905b4afa350d3c7ef
Bhupendra Atre <bhupendra.atre@gmail.com>
2018-01-28 15:50:23 -0500

    amzn-nodejs-splunk-pci: fix forever

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 #start splunk
-splunk
+#splunk
 
 #start node app
-forever start                               &&\
-        -l /var/log/app/forever.log         &&\
-        -o /var/log/app/forever-out.log     &&\
-        -e /var/log/app/forever-err.log     &&\
-        -c node '--max-old-space-size=1000' &&\
+forever -l /var/log/app/forever.log \
+        -o /var/log/app/forever-out.log \
+        -e /var/log/app/forever-err.log \
+        -c node '--max-old-space-size=1000' \
         server.js
