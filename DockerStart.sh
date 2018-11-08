02e7963856bc774ff5aae47567da3a22c3919c5a
Bhupendra Atre <bhupendra.atre@gmail.com>
2018-11-08 12:45:24 -0500

    fix newrelic condition

--- DockerStart.sh
+++ DockerStart.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-if [ -z NEW_RELIC_APP_NAME -a -z NEW_RELIC_LICENSE_KEY ]; then sed -i "s|env node|env node\\nrequire('newrelic');\\n|" server.js; fi &&\
-if [ -z NEW_RELIC_APP_NAME -a -z NEW_RELIC_LICENSE_KEY ]; then echo "exports.config = {app_name:['$NEW_RELIC_APP_NAME'], license_key:'$NEW_RELIC_LICENSE_KEY', logging:{level:'info'}}" > newrelic.js; fi
+if [ -n "$NEW_RELIC_APP_NAME" -a -n "$NEW_RELIC_LICENSE_KEY" ]
+then 
+  sed -i "s|env node|env node\\nrequire('newrelic');\\n|" server.js;
+  echo "exports.config = {app_name:['$NEW_RELIC_APP_NAME'], license_key:'$NEW_RELIC_LICENSE_KEY', logging:{level:'info'}}" > newrelic.js;
+else
+  echo "skipping newrelic";
+fi &&\
 /home/app/node_modules/pm2/bin/pm2 start --no-daemon -s pm2.config.js
